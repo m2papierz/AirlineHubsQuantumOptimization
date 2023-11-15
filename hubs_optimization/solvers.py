e64dea81c87a48277190ea4ac1f649681bb46720
m2papierz <m2papierz@gmail.com>
2023-11-15 15:21:31 +0100

    Add placeholders for different solvers

---
+++ hubs_optimization/solvers.py
@@ -0,0 +1,62 @@
+from typing import Union, TypeAlias
+from abc import abstractmethod
+from dimod import DiscreteQuadraticModel
+from dimod import ConstrainedQuadraticModel
+from dwave.system import LeapHybridDQMSampler
+from dwave.system import LeapHybridCQMSampler
+
+
+QuadraticModel: TypeAlias = Union[DiscreteQuadraticModel, ConstrainedQuadraticModel]
+
+
+class QuadraticModelSolver:
+    def __init__(self):
+        self.solver = self._initiate_solver()
+
+    @abstractmethod
+    def _initiate_solver(self):
+        pass
+
+    @abstractmethod
+    def sample(self, quadratic_model: QuadraticModel):
+        pass
+
+
+class AnnealingSolver(QuadraticModelSolver):
+    def __init__(self, discrete: bool = False):
+        self.discrete = discrete
+        super().__init__()
+
+    def _initiate_solver(self):
+        if self.discrete:
+            return LeapHybridDQMSampler()
+        else:
+            return LeapHybridCQMSampler()
+
+    def sample(self, quadratic_model: QuadraticModel):
+        if self.discrete:
+            return self.solver.sample_dqm(quadratic_model)
+        else:
+            return self.solver.sample_cqm(quadratic_model)
+
+
+class ApproximateSolver(QuadraticModelSolver):
+    def __init__(self):
+        super().__init__()
+
+    def _initiate_solver(self):
+        pass
+
+    def sample(self, quadratic_model: QuadraticModel):
+        pass
+
+
+class GroverAdaptiveSolver(QuadraticModelSolver):
+    def __init__(self):
+        super().__init__()
+
+    def _initiate_solver(self):
+        pass
+
+    def sample(self, quadratic_model: QuadraticModel):
+        pass
