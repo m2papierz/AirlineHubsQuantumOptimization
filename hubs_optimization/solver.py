e13ce91f1b11a2d64d01c6a26b7504adcbc45a3a
m2papierz <m2papierz@gmail.com>
2023-11-16 22:04:25 +0100

    Fix solver labels

--- hubs_optimization/solver.py
+++ hubs_optimization/solver.py
@@ -1,32 +1,16 @@
 from typing import Union, TypeAlias
-from abc import abstractmethod
 from dimod import DiscreteQuadraticModel
 from dimod import ConstrainedQuadraticModel
 from dwave.system import LeapHybridDQMSampler
 from dwave.system import LeapHybridCQMSampler
 
-
 QuadraticModel: TypeAlias = Union[DiscreteQuadraticModel, ConstrainedQuadraticModel]
 
 
-class QuadraticModelSolver:
-    def __init__(self):
-        self.solver = self._initiate_solver()
-
-    @abstractmethod
-    def _initiate_solver(self):
-        pass
-
-    @abstractmethod
-    def sample(self, quadratic_model: QuadraticModel):
-        pass
-
-
-class AnnealingSolver(QuadraticModelSolver):
+class QuadraticModelAnnealingSolver:
     def __init__(self, discrete: bool = False):
         self.discrete = discrete
-        self.label = 'CQM Airline Hubs'
-        super().__init__()
+        self.solver = self._initiate_solver()
 
     def _initiate_solver(self):
         if self.discrete:
@@ -37,7 +21,7 @@ class AnnealingSolver(QuadraticModelSolver):
     def sample(self, quadratic_model: QuadraticModel):
         if self.discrete:
             return self.solver.sample_dqm(
-                quadratic_model, label=self.label)
+                quadratic_model, label='DQM Airline Hubs')
         else:
             return self.solver.sample_cqm(
-                quadratic_model, label=self.label)
+                quadratic_model, label='CQM Airline Hubs')
